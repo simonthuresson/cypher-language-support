6930889bda915a558bd0a84b849915a6ad3e0d8e
simonthuresson <sthuresson00@gmail.com>
2025-03-25 18:09:06 +0100

    foreach correctly indents paranthesis

--- packages/language-support/src/formatting/formatting.ts
+++ packages/language-support/src/formatting/formatting.ts
@@ -1985,10 +1985,10 @@ export class TreePrintVisitor extends CypherCmdParserVisitor<void> {
     for (let i = 0; i < n; i++) {
       this.addBaseIndentation();
       this._visit(ctx.clause(i));
+      this.removeBaseIndentation();
     }
     this.breakLine();
     this._visit(ctx.RPAREN());
-    this.removeBaseIndentation();
   };
 
   visitProcedureName = (ctx: ProcedureNameContext) => {
